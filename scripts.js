06b320c94060f0e8afeda60e75b3084c9f61a4b4
ddutta98 <32028770+ddutta98@users.noreply.github.com>
2020-12-20 18:11:29 +0530

    Scripts for original Portuguese added

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 function display() {
-    //please translate to English before running the scripts
+    // After translating, the dates in text nodes get replaced by <font> tags, so I'm creating separate scripts for the Portuguese and English
     
+    //For English Translation
+    //-----------------------------------------------------------------------------------------------------------------------
     //Publication Date
     console.log(document.querySelector("#block-views-block-view-noticia-pbh-block-5 .views-field-nothing span span :nth-child(2)").innerText);
     
@@ -12,10 +14,37 @@ function display() {
     
     //Links
     let x = document.querySelectorAll("#block-views-block-view-noticia-pbh-block-5 .views-field-field-historico-da-licitacao tbody tr :nth-child(2) a");
-
     let allLinks = '';
     [].forEach.call(x, function (x) {
         allLinks = allLinks.concat(x.href, "\n");
     });
     console.log(allLinks);
+    
+    //For original page in Portuguese (Scripts for object and links are same)
+    //------------------------------------------------------------------------------------------------------------------------
+    //Publication Date
+    var cloned =document.querySelector("#block-views-block-view-noticia-pbh-block-5 .views-field-nothing span span").cloneNode( true );
+    var span=cloned.childNodes[0];
+    var textnode = document.createTextNode('');
+    cloned.replaceChild(textnode, span);
+    console.log(cloned.innerText);
+    
+    //Object
+    console.log(document.querySelector("#block-views-block-view-noticia-pbh-block-5 .views-field-nothing span p").innerText);
+
+    //Bidding Date
+    var cloned2 =document.querySelectorAll("#block-views-block-view-noticia-pbh-block-5 .field-content>span")[6].cloneNode( true );
+    var span2=cloned2.childNodes[0];
+    var textnode2 = document.createTextNode('');
+    cloned2.replaceChild(textnode2, span2);
+    console.log(cloned2.innerText);
+    
+    //Links
+    let x = document.querySelectorAll("#block-views-block-view-noticia-pbh-block-5 .views-field-field-historico-da-licitacao tbody tr :nth-child(2) a");
+    let allLinks = '';
+    [].forEach.call(x, function (x) {
+        allLinks = allLinks.concat(x.href, "\n");
+    });
+    console.log(allLinks);
+    
 }
