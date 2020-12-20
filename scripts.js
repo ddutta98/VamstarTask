21fd3763a6b3f61a7894002bb8be8e231e0d91f0
ddutta98 <32028770+ddutta98@users.noreply.github.com>
2020-12-20 13:36:33 +0530

    Added comments

--- scripts.js
+++ scripts.js
@@ -1,12 +1,16 @@
 function display() {
     //please translate to English before running the scripts
+    
+    //Publication Date
     console.log(document.querySelector("#block-views-block-view-noticia-pbh-block-5 .views-field-nothing span span :nth-child(2)").innerText);
-
+    
+    //Object
     console.log(document.querySelector("#block-views-block-view-noticia-pbh-block-5 .views-field-nothing span p").innerText);
 
-
+    //Bidding Date
     console.log(document.querySelectorAll("#block-views-block-view-noticia-pbh-block-5 .views-field-nothing span>font")[9].innerText);
-
+    
+    //Links
     let x = document.querySelectorAll("#block-views-block-view-noticia-pbh-block-5 .views-field-field-historico-da-licitacao tbody tr :nth-child(2) a");
 
     let allLinks = '';
